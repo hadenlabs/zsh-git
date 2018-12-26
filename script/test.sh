a08163b4796abe2ac031e354f49b36959c5e98d4
@slovacus <slovacus@gmail.com>
2018-12-25 22:36:26 -0500

    (#8) Fix: clean files unnecesary

--- script/test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# -*- coding: utf-8 -*-
-
-# shellcheck source=/dev/null
-[ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
-
-ansible-playbook tests/test.yaml -i tests/inventory --syntax-check
-ansible-playbook -i tests/inventory tests/test.yaml --connection=local
