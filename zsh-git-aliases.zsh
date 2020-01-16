391c649db09f8e8d41e00eef56331a4cbb28b660
@slovacus <slovacus@gmail.com>
2020-01-16 07:41:33 -0500

    refactor: separated logics (#38)

--- zsh-git-aliases.zsh
+++ zsh-git-aliases.zsh
@@ -13,13 +13,24 @@
 #
 
 ZSH_GIT_ALIASES_ROOT_PATH=$(dirname "${0}")
-ZSH_GIT_ALIASES_HOOKS_PATH="${ZSH_GIT_ALIASES_ROOT_PATH}/src/git/hooks/"
+ZSH_GIT_ALIASES_SRC_PATH="${ZSH_GIT_ALIASES_ROOT_PATH}/src"
+ZSH_GIT_ALIASES_HOOKS_PATH="${ZSH_GIT_ALIASES_SRC_PATH}/git/hooks/"
 ZSH_GIT_REGEX_IS_HOOK="^(prepare-commit-msg)"
 ZSH_GIT_REGEX_DOMAIN_ENABLED="(github.com|bitbucket.org)"
 
 GITHUB_USER="$(git config github.user)"
 BITBUCKET_USER="$(git config bitbucket.user)"
 GITLAB_USER="$(git config gitlab.user)"
+
+# shellcheck source=/dev/null
+source "${ZSH_GIT_ALIASES_SRC_PATH}"/base.zsh
+
+# shellcheck source=/dev/null
+source "${ZSH_GIT_ALIASES_SRC_PATH}"/git.zsh
+
+# shellcheck source=/dev/null
+source "${ZSH_GIT_ALIASES_SRC_PATH}"/alias.zsh
+
 
 function git::dependences::check {
     if [ -z "${GITHUB_USER}" ]; then
@@ -34,132 +45,6 @@ function git::dependences::check {
         message_warning "You should set 'git config --global gitlab.user'."
     fi
 }
-
-# exist_hook: validate when exist hook in path src/git/hooks
-function git::exist_hook {
-    local hook_name
-    hook_name="${1}"
-    echo "${hook_name}" | grep -cE "${ZSH_GIT_REGEX_IS_HOOK}"; echo
-}
-
-# has_hook: validate if have installed hook
-function git::has_hook {
-    local hook_name
-    hook_name="${1}"
-    if [ -e .git/hooks/"${hook_name}" ]; then
-        echo -e 1
-    fi
-}
-
-# copy_hook: copy a hook to project git
-function git::copy_hook {
-    local hook_name
-    local has_hook
-    hook_name="${1}"
-    has_hook=$(git::exist_hook "${hook_name}")
-    if [ "${has_hook}" -eq 1 ]; then
-        [ -e .git/hooks ] && cp -rf "${ZSH_GIT_ALIASES_HOOKS_PATH}/${hook_name}" .git/hooks/
-        message_success "copy hook ${hook_name}"
-    else
-        message_warning "not found hook ${hook_name}"
-    fi
-}
-
-function git::hook::factory {
-    local hook_name
-    hook_name=prepare-commit-msg
-    if [ "$(git::has_hook ${hook_name})" -eq 0 ]; then
-        git::copy_hook "${hook_name}"
-    fi
-}
-
-alias gl='git pull'
-alias gp='git push'
-alias gau='git add --update'
-alias gc='git commit -v'
-alias gca='git commit -v -a'
-alias gb='git branch'
-alias gba='git branch -a'
-alias gco='git checkout'
-alias gcob='git checkout -b'
-alias gcot='git checkout -t'
-alias gcotb='git checkout --track -b'
-alias glog='git log'
-alias glogp='git log --pretty=format:"%h %s" --graph'
-
-# Git Flow
-
-alias gf='git flow'
-alias gfr='git flow release'
-alias gfh='git flow hotfix'
-
-function git::branch::name {
-    local branch_name
-    branch_name="$(git symbolic-ref --short HEAD)"
-    echo "${branch_name}"
-}
-
-function git::branch::task_name {
-    local branch_name
-    branch_name="$(git::branch::name)"
-    branch_name="${branch_name##*/}"
-    echo "${branch_name}"
-}
-
-function git::branch::is_develop {
-    if [ "$(git::branch::name)" = "develop" ]; then
-        echo 1
-        return
-    fi
-    echo 0
-}
-
-# git::repository::remote::url -> string
-# input remote_name
-## return url of repository remote
-function git::repository::remote::url {
-    local remote_name
-    remote_name="${1}"
-    git remote get-url "${remote_name}" 2>/dev/null || echo; echo
-}
-
-# git::repository::fork::private
-#  return true when origign is different to upstream
-function git::repository::fork::private {
-    local domain_origin
-    local domain_upstream
-    domain_origin=$(echo "$(git::repository::remote::url origin)" | grep -Eo "${ZSH_GIT_REGEX_DOMAIN_ENABLED}")
-    domain_upstream=$(echo "$(git::repository::remote::url upstream)" | grep -Eo "${ZSH_GIT_REGEX_DOMAIN_ENABLED}")
-    if [ -z "${domain_upstream}" ]; then
-        echo 0
-        return
-    fi
-
-    if [ "${domain_origin}" != "${domain_upstream}" ]; then
-        echo 1
-        return
-    fi
-    echo 0
-}
-
-function gff::publish {
-    local branch_name
-    branch_name="$(git::branch::name)"
-    if [ "$(git::repository::fork::private)" -eq 1 ]; then
-        git push upstream "${branch_name}"
-    else
-        git push origin "${branch_name}"
-    fi
-}
-function gff::git::sync {
-    message_info "starting sync branchs"
-    if [ "$(git::branch::is_develop)" -eq 0 ]; then
-        git checkout develop
-    fi
-    git-sync
-    message_success "finish sync branchs"
-}
-
 
 function gff {
     local action
@@ -186,17 +71,3 @@ function gff {
     fi
 
 }
-
-function git::dependences::install {
-    if ! type -p brew > /dev/null; then
-        message_warning "please install brew to continue or add zshrc luismayta/zsh-brew"
-    else
-        if ! type -p hub > /dev/null; then
-            message_info "Installing hub"
-            brew install hub
-            message_success "Installed hub"
-        fi
-    fi
-}
-
-git::dependences::install
