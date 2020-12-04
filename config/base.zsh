06db26670f70a057fac9d740ba7a4bd7f3a5b2a7
@slovacus <slovacus@gmail.com>
2020-12-04 01:52:50 -0500

    refactor: load config for environment (#65)

---
+++ config/base.zsh
@@ -0,0 +1,10 @@
+#!/usr/bin/env ksh
+# -*- coding: utf-8 -*-
+
+export GIT_MESSAGE_BREW="Please install brew or use antibody bundle luismayta/zsh-brew branch:develop"
+export GIT_MESSAGE_RVM="Please install rvm or use antibody bundle luismayta/zsh-rvm branch:develop"
+export GIT_PACKAGE_NAME=git
+
+export ZSH_GIT_HOOKS_PATH="${ZSH_GIT_PATH}/template/git/hooks/"
+export ZSH_GIT_REGEX_IS_HOOK="^(prepare-commit-msg)"
+export ZSH_GIT_REGEX_DOMAIN_ENABLED="(github.com|bitbucket.org)"
