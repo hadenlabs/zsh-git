896dde899bdf22ab45c2f67dc074fb4d5ead4473
Luis Mayta <luis@hadenlabs.com>
2020-12-07 16:48:36 -0500

    refactor: read file config of git (#59)

--- pkg/helper.zsh
+++ pkg/helper.zsh
@@ -1,8 +1,17 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
 
-# editgit edit settings for git
+# editgitconfig edit settings for git
 function editgit {
+    if [ -z "${EDITOR}" ]; then
+        message_warning "it's neccesary the var EDITOR"
+        return
+    fi
+    "${EDITOR}" "$(git-root)/.git/config"
+}
+
+# editgitconfigglobal edit settings for git global
+function editgitglobal {
     if [ -z "${EDITOR}" ]; then
         message_warning "it's neccesary the var EDITOR"
         return
