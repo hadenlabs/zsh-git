adcfce431298cc2dcc9f515cff318785cfc75bef
Luis Mayta <luis@hadenlabs.com>
2020-12-20 18:50:41 -0500

    chore: implement vars conf path for git (#58)

--- config/base.zsh
+++ config/base.zsh
@@ -1,6 +1,7 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
 export GIT_FILE_SETTINGS="${HOME}"/.gitconfig
+export GIT_CONF_PATH="${ZSH_GIT_PATH}/conf"
 export GIT_MESSAGE_BREW="Please install brew or use antibody bundle luismayta/zsh-brew branch:develop"
 export GIT_MESSAGE_RVM="Please install rvm or use antibody bundle luismayta/zsh-rvm branch:develop"
 export GIT_PACKAGE_NAME=git
