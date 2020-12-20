1366679b9935fc35cc1ccaef8b564877d3a0d57d
@slovacus <slovacus@gmail.com>
2020-12-20 16:21:38 -0500

    chore: implement internal functions git (#58)

--- internal/helper.zsh
+++ internal/helper.zsh
@@ -1,2 +1,10 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
+
+function git::internal::git::user {
+    git config user.name
+}
+
+function git::internal::git::email {
+    git config user.email
+}
