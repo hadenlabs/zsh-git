31a1d5a5c23b9c0e3b0f2a4a6e3bde7f20dbbcae
Luis Mayta <luis@hadenlabs.com>
2024-02-02 11:51:40 -0300

    fix: implement push origin gwip (#120)

--- pkg/alias.zsh
+++ pkg/alias.zsh
@@ -15,7 +15,7 @@ alias gcotb='git checkout --track -b'
 alias glog='git log'
 alias glogp='git log --pretty=format:"%h %s" --graph'
 
-alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify --no-gpg-sign --message "--wip-- [skip ci]"'
+alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify --no-gpg-sign --message "--wip-- [skip ci]"; git push origin $(git rev-parse --abbrev-ref HEAD)'
 
 alias gunwip='git rev-list --max-count=1 --format="%s" HEAD | grep -q "\--wip--" && git reset HEAD~1'
 
