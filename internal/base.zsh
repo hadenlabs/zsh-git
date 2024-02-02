79fa56e7997664c55bb9b6ef177766cb4ca306bd
Luis Mayta <luis@hadenlabs.com>
2024-02-02 10:51:29 -0300

    chore: implement alias for gunwip (#120)

--- internal/base.zsh
+++ internal/base.zsh
@@ -43,3 +43,14 @@ function git::internal::provision::hooks::sync {
     fi
     rsync -avP "${ZSH_GIT_HOOKS_PATH}/" "${GIT_PROVISION_HOOKS_PATH}/"
 }
+
+# Similar to `gunwip` but recursive "Unwips" all recent `--wip--` commits not just the last one
+function git::internal::gunwipall {
+  local _commit
+  _commit="$(git log --grep='--wip--' --invert-grep --max-count=1 --format=format:%H)"
+
+  # Check if a commit without "--wip--" was found and it's not the same as HEAD
+  if [[ "${_commit}" != "$(git rev-parse HEAD)" ]]; then
+    git reset "${_commit}" || return 1
+  fi
+}
